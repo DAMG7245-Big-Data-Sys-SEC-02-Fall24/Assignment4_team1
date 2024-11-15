86a63489fe8304deb423ca57460f3aa5560daa50
Sai Surya Madhav <rebbapragada.s@northeastern.edu>
2024-11-15 14:26:04 -0500

    clean

--- backend/run_service.py
+++ backend/run_service.py
@@ -5,6 +5,9 @@ from dotenv import load_dotenv
 load_dotenv()
 
 if __name__ == "__main__":
+    
+
+
     if os.getenv("MODE") != "dev":
         from backend.service.service import app
         uvicorn.run(app, host="0.0.0.0", port=8000)
